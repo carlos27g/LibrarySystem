18fd949799371a393753e72f54f4be5d63f32068
carlos27g <carlos.garzon@tum.de>
2023-01-03 14:31:05 +0000

    Springboot initialized

---
+++ src/main/java/TransportProblem/TransportProblemApplication.java
@@ -0,0 +1,13 @@
+package TransportProblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportProblemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TransportProblem/TransportProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package TransportProblem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
